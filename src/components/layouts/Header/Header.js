e43dd8002a9fb5e93b1f00e70f37fede9620b458
M Usama Ramzan <usamaramzan215@gmail.com>
2025-11-29 18:37:24 +0500

    refactor: update button styles and functionality in Header and Sidebar components for improved user experience

--- src/components/layouts/Header/Header.js
+++ src/components/layouts/Header/Header.js
@@ -1,3 +1,163 @@
+// import React, { useState, Fragment } from "react";
+// import styles from "./Header.module.scss";
+// import Layout from "../Layout/Layout";
+// import { Button, Link, Logo } from "../../elements";
+// // import THEME from "../../../state/theme";
+// import { useCustomState } from "../../../state/state";
+
+// export default ({ data = [] }) => {
+//   const [sticky, setSticky] = useState(true);
+//   const actions = useCustomState()[1];
+
+//   // Dutch translations fallback (if data is not loaded from mock.js)
+//   const dutchMenu = [
+//     {
+//       name: "Home page",
+//       url: "/",
+//       children: [
+//         // { name: "Slider Header", url: "/" },
+//         // { name: "Parallax Image", url: "/home-paralax" },
+//         // { name: "About us", url: "/about-us" },
+//         // { name: "Video Background", url: "/home-video" },
+//         { name: "Why us", url: "/why-us" },
+//         { name: "How we work", url: "/how-we-work" },
+//         { name: "Start working", url: "/working-us" },
+//       ],
+//     },
+//     { name: "About us", url: "/about-us" },
+//     { name: "Services", url: "/services" },
+//     { name: "Portfolio", url: "/portfolio" },
+//     // {
+//     //   name: "Blogging",
+//     //   url: "/blog-grid-left-sidebar",
+//     //   children: [
+//     //     { name: "Grid Left Sidebar", url: "/blog-grid-left-sidebar" },
+//     //     { name: "List Links Sidebar", url: "/blog-list-left-sidebar" },
+//     //     { name: "Grid Right Sidebar", url: "/blog-grid-right-sidebar" },
+//     //     { name: "List Right Sidebar", url: "/blog-list-right-sidebar" },
+//     //     { name: "Grid No Sidebar", url: "blog-grid-without-sidebar" },
+//     //   ],
+//     // },
+//     {
+//       name: "Contacts", url: "/contacts",
+//       children:
+//         [
+//           { name: "Consultation", url: "/consultation" },
+//         ]
+//     },
+//   ];
+
+//   const menuData = data.length > 0 ? data : dutchMenu;
+
+//   const menu = menuData.map((item, index) => {
+//     if (!item.children) {
+//       return (
+//         <li key={index}>
+//           <Link url={item.url} className={styles.menuLink}>
+//             {item.name}
+//           </Link>
+//         </li>
+//       );
+//     } else {
+//       return (
+//         <li key={index}>
+//           <span>
+//             <Link url={item.url} className={styles.menuLink}>
+//               {item.name}
+//             </Link>
+//           </span>
+//           <ul>
+//             {item.children.map((subitem, subindex) => (
+//               <li key={subindex}>
+//                 <a href={subitem.url}>{subitem.name}</a>
+//               </li>
+//             ))}
+//           </ul>
+//         </li>
+//       );
+//     }
+//   });
+
+//   const header = (
+//     <Layout>
+//       <div className={styles.header}>
+//         <Link url="/">
+//           <Logo dark />
+//         </Link>
+
+//         <ul className={styles.menu}>{menu}</ul>
+
+//          <div className={styles.btn_desktop}>
+//           <Button
+//             after="&#xf107;"
+//             // ✅ Use the new types we just created
+//             type="outline-orange-ot"      // This is our new default style
+//             hoverType="solid-orange-wt"  // This is our new hover style
+//             click={() => actions.toogleModal()}
+//           >
+//             plan een consult
+//           </Button>
+//         </div>
+
+
+//         {/* <Button
+//             after="&#xf107;"
+//             type="outline-white-tw"
+//             hoverType="solid-white-tb"
+//             click={() => actions.toogleModal()}
+//           >
+//             Get Quote
+//           </Button> */}
+
+//         <div className={styles.btn_desktop}>
+//           <Button
+//             after="&#xf107;"
+//             // ✅ Use the new types we just created
+//             type="outline-orange-ot"      // This is our new default style
+//             hoverType="solid-orange-wt"  // This is our new hover style
+//             click={() => actions.toogleModal()}
+//           >
+//             Offerte aanvragen
+//           </Button>
+//         </div>
+
+//         <div
+//           // style={{ color: "#000000" }}
+//           className={styles.btn_mobile}
+//           onClick={() => actions.toogleSidebar()}
+//         >
+//           <Button
+//             after="&#xf0c9;"
+//             type="outline-color-tw"
+//             hoverType="solid-color-tw"
+//           //  style={{ color: "#000000", borderColor: "#000000" }}
+//           />
+//         </div>
+//       </div>
+//     </Layout>
+//   );
+
+//   return (
+//     <Fragment>
+//       <div className={styles.wrapper}>{header}</div>
+//       <div
+//         className={[
+//           styles.wrapper,
+//           sticky ? styles.sticky : styles.hidden,
+//         ].join(" ")}
+//       >
+//         {header}
+//       </div>
+//     </Fragment>
+//   );
+// };
+
+
+
+
+
+
+
 import React, { useState, Fragment } from "react";
 import styles from "./Header.module.scss";
 import Layout from "../Layout/Layout";
@@ -87,22 +247,25 @@ export default ({ data = [] }) => {
 
         <ul className={styles.menu}>{menu}</ul>
 
-
-        {/* <Button
+        {/* --- BUTTON 1: Plan een consult (Goes to Page) --- */}
+        <div className={styles.btn_desktop} style={{ marginRight: '15px' }}>
+          {/* Removed Link wrapper, added click prop directly to Button */}
+          <Button
             after="&#xf107;"
-            type="outline-white-tw"
-            hoverType="solid-white-tb"
-            click={() => actions.toogleModal()}
+            type="outline-orange-ot"      
+            hoverType="solid-orange-wt"  
+            click={() => window.location.href = "/consultation"} 
           >
-            Get Quote
-          </Button> */}
+            Plan een consult
+          </Button>
+        </div>
 
+        {/* --- BUTTON 2: Offerte aanvragen (Opens Modal) --- */}
         <div className={styles.btn_desktop}>
           <Button
             after="&#xf107;"
-            // ✅ Use the new types we just created
-            type="outline-orange-ot"      // This is our new default style
-            hoverType="solid-orange-wt"  // This is our new hover style
+            type="outline-orange-ot"
+            hoverType="solid-orange-wt"
             click={() => actions.toogleModal()}
           >
             Offerte aanvragen
--- src/components/layouts/Sidebar/Sidebar.js
+++ src/components/layouts/Sidebar/Sidebar.js
@@ -101,6 +101,131 @@
 
 
 
+// import React, { useState } from "react";
+// import styles from "./Sidebar.module.scss";
+// import { useCustomState } from "../../../state/state";
+// import { Button, Link, Logo } from "../../elements";
+// import { Backdrop } from "../../ui";
+// import THEME from "../../../state/theme";
+
+// export default ({ data = [] }) => {
+//   const [state, actions] = useCustomState();
+//   const [submenu, setSubmenu] = useState(null);
+
+//   const hover = {
+//     color: THEME.color,
+//   };
+
+//   const toogleSubmenu = (index) => {
+//     index === submenu ? setSubmenu(null) : setSubmenu(index);
+//   };
+
+//   const toogleSidebar = () => {
+//     actions.toogleSidebar();
+//     state.backdrop && actions.toogleBackdrop();
+//   };
+
+//   const toogleModal = () => {
+//     actions.toogleModal();
+//     actions.toogleSidebar();
+//   };
+
+//   const menu = data.map((item, index) => {
+//     if (!item.children) {
+//       return (
+//         <li key={index}>
+//           <Link click={toogleSidebar} url={item.url} hoverStyle={hover}>
+//             {item.name}
+//           </Link>
+//         </li>
+//       );
+//     } else {
+//       return (
+//         <li key={index}>
+//           <span style={{ position: "relative", display: "block" }}>
+//             {/* 1. The Text Link: Navigates to the page */}
+//             <Link 
+//               url={item.url || "#"} 
+//               hoverStyle={hover} 
+//               click={toogleSidebar}
+//             >
+//               {item.name}
+//             </Link>
+
+//             {/* 2. The Arrow Icon: Toggles the submenu independently */}
+//             <i
+//               className={
+//                 submenu === index ? "las la-angle-up" : "las la-angle-down"
+//               }
+//               onClick={(e) => {
+//                 e.preventDefault();
+//                 e.stopPropagation();
+//                 toogleSubmenu(index);
+//               }}
+//               style={{ cursor: "pointer", zIndex: 10 }}
+//             />
+//           </span>
+          
+//           <ul
+//             style={{
+//               height:
+//                 submenu === index ? 30 * item.children.length + "px" : "0",
+//             }}
+//           >
+//             {item.children.map((subitem, subindex) => (
+//               <li key={subindex}>
+//                 <Link
+//                   click={toogleSidebar}
+//                   url={subitem.url}
+//                   hoverStyle={hover}
+//                 >
+//                   {subitem.name}
+//                 </Link>
+//               </li>
+//             ))}
+//           </ul>
+//         </li>
+//       );
+//     }
+//   });
+
+//   return (
+//     <div className={[styles.wrapper, state.sidebar && styles.show].join(" ")}>
+//       <div className={styles.sidebar}>
+//         <div className={styles.btn_close} onClick={toogleSidebar}>
+//           <Button
+//             after="&#xf00d;"
+//             type="solid-white-tb"
+//             hoverType="solid-gray-tb"
+//           />
+//         </div>
+//         <div className={styles.logo}>
+//           <Logo />
+//         </div>
+
+//         <ul className={styles.menu}>{menu}</ul>
+        
+        
+//         <div className={styles.btn_contacts}>
+//           <Button hoverType="solid-gray-tb" click={toogleModal}>
+//           plan een consult
+//           </Button>
+//           <br></br>
+//           <Button hoverType="solid-gray-tb" click={toogleModal}>
+//             Offerte aanvragen
+//           </Button>
+//         </div>
+//       </div>
+//       <Backdrop />
+//     </div>
+//   );
+// };
+
+
+
+
+
+
 import React, { useState } from "react";
 import styles from "./Sidebar.module.scss";
 import { useCustomState } from "../../../state/state";
@@ -206,9 +331,24 @@ export default ({ data = [] }) => {
         <ul className={styles.menu}>{menu}</ul>
         
         <div className={styles.btn_contacts}>
-          <Button hoverType="solid-gray-tb" click={toogleModal}>
-            Offerte aanvragen
-          </Button>
+          {/* Button 1: Plan een consult - Navigates to Page */}
+          <div style={{ marginBottom: '1rem' }}>
+            <Button 
+              hoverType="solid-gray-tb" 
+              click={() => {
+                window.location.href = "/consultation";
+              }}
+            >
+              Plan een consult
+            </Button>
+          </div>
+
+          {/* Button 2: Offerte aanvragen - Opens Modal */}
+          <div>
+            <Button hoverType="solid-gray-tb" click={toogleModal}>
+              Offerte aanvragen
+            </Button>
+          </div>
         </div>
       </div>
       <Backdrop />
